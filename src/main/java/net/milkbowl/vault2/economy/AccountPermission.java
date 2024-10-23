fc0f108972be6e825c2b00c8308ac5fece232fc5
Daniel V. <daniel.viddy@gmail.com>
2024-10-23 17:37:17 -0400

    Update license header to LGPL for AccountPermission.

--- src/main/java/net/milkbowl/vault2/economy/AccountPermission.java
+++ src/main/java/net/milkbowl/vault2/economy/AccountPermission.java
@@ -1,27 +1,26 @@
 package net.milkbowl.vault2.economy;
-/*
- * The New Kings
- * Copyright (C) 2022 - 2024 Daniel "creatorfromhell" Vidmar
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Affero General Public License for more details.
- *
- * You should have received a copy of the GNU Affero General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+/* This file is part of Vault.
+
+    Vault is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Vault is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with Vault.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 /**
  * AccountPermission
  *
  * @author creatorfromhell
- * @since 0.0.1.0
+ * @since 2.7
  */
 public enum AccountPermission {
 
